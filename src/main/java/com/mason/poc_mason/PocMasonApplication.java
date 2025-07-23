0a1ba9f9f52f9da2023f8cdc9cf9fb20d0990492
Alpha Diallo <al.diallo@groupeonepoint.com>
2025-07-23 15:28:31 +0200

    init demo

---
+++ src/main/java/com/mason/poc_mason/PocMasonApplication.java
@@ -0,0 +1,13 @@
+package com.mason.poc_mason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMasonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMasonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mason/poc_mason/PocMasonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mason.poc_mason;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMasonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
